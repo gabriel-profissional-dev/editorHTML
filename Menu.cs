96fae2b4fc72b561e2c83e059dd5590381c3e138
Gabriel <gabriel.profissional.dev@gmail.com>
2022-07-25 22:18:09 -0300

    iniciando projeto 2.0

---
+++ Menu.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace editorhtml
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;//Cor de fundo azul
+            Console.ForegroundColor = ConsoleColor.Black;//Cor da letra preto
+
+            
+        }
+    }
+}
