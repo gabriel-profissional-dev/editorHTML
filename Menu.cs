432c6a4871d8f7e9219d2549d1d0209f92ca4426
Gabriel <gabriel.profissional.dev@gmail.com>
2022-07-26 21:44:18 -0300

    Desenhando a Tela

--- Menu.cs
+++ Menu.cs
@@ -8,9 +8,40 @@ namespace editorhtml
         {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;//Cor de fundo azul
-            Console.ForegroundColor = ConsoleColor.Black;//Cor da letra preto
+            Console.ForegroundColor = ConsoleColor.Black;//Cor da letra preto     
 
-            
+            DrawScreen();
+        }
+
+        public static void DrawScreen()//Função criada para desenhar a tela
+        {
+            Console.Write("+");
+            for (int i = 0; i <=30; i++ )
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+            for (int lines = 0; lines <= 10; lines ++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <=30; i++ )
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            Console.Write("+");
+            for (int i = 0; i <=30; i++ )
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
         }
     }
 }
